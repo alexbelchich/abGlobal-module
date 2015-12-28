830701bd333eb1693ab0db5f884f1c53d1a8f557
alexbelchich <alex.belchich@gmail.com>
2015-12-28 14:07:50 +0300

    changed done page

--- src/app/screens/done/done.controller.js
+++ src/app/screens/done/done.controller.js
@@ -6,7 +6,7 @@
         .controller('DoneController', DoneController);
 
     /** @ngInject */
-    function DoneController($scope, NavigationService, ObservationsService, DiscoveryService) {
+    function DoneController($scope, NavigationService, ObservationsService, DiscoveryService, $log) {
         var vm = this;
         var listener = $scope.$on('willDisplayNextScreen', function () {
             vm.finishModule();
@@ -28,5 +28,11 @@
 
             DiscoveryService.finishedModule(observations, metas, applicant, state);
         };
+
+        vm.changeBg = function () {
+            angular.element('body').css('background-color', '#1bd830');
+        };
+
+        vm.changeBg();
     }
 })();
